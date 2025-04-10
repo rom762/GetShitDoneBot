93ce5b00584a11b3d823f0c8cf57ba26a73eaeac
Roman Nikulin <roman.nikulin@glowbyteconsulting.com>
2025-04-10 12:37:08 +0300

    new prompt

--- settings.py
+++ settings.py
@@ -1,13 +1,25 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv('.env')
+load_dotenv('.env.sample')
 
 api_key = os.getenv('OPENAI_TOKEN')
 base_url = os.getenv('BASE_URL')
 telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
 prompts = {'basic': 'You are a helpful assistant',
-           'main': "Please analyze the following text and identify the task's meaning, deadlines, and category according to GTD principles. If you need more information, please ask a clarifying question."}
+           'main': "Please analyze the following text and identify the task's meaning, deadlines, and category according to GTD principles. If you need more information, please ask a clarifying question.",
+           'main_v1': "Please analyze the following text and determine the meaning of "
+                      "the task, the time frame for its completion and the category in accordance with "
+                      "the principles of GTD. "
+                      "Answer in special format: "
+                      "Task:"
+                      "Deadline:"
+                      "Category:"
+                      "If Username is present in the following text, use it as a Category. Finally make a json with keys Task, Deadline, Category, Username. if you don't have enough data, request it from the user."
+                      "If you need more information, please ask a clarifying question.",
+           'main_v2': "Please analyze the following text and identify the task's meaning, deadlines,"
+                      " and category according to GTD principles. Finally make a json with keys Task, Deadline, Category, Username. if you don't have enough data, request it from the user."
+           }
 
 if __name__ == "__main__":
     print(f'OPENAI_API_KEY:{api_key}')
